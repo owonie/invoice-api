9d381d7ae1e2a97a5f9f0294046296ea975699e0
Owon <chlehdnjs@gmail.com>
2024-01-03 00:26:57 +0900

    init: Initialization

---
+++ src/main/kotlin/com/example/invoiceapi/InvoiceApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.invoiceapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvoiceApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<InvoiceApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/invoiceapi/InvoiceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.invoiceapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvoiceApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
